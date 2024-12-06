da013f04f27187bd8565fd2454674dadea0ca521
Eliezer Guevara <magdiel.852@gmail.com>
2024-12-05 18:02:45 -0600

    Add CORS support, enhance error handling, and implement request validation for product routes

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const ProductsService= require('./../services/product.service')
+const ProductsService = require('./../services/product.service');
+const validatorHandler = require('../middlewares/validator.handler');
+const {
+  createProductSchema,
+  updateProductSchema,
+  getProductSchema,
+} = require('../schemas/product.schema');
 
 const router = express.Router();
 const service = new ProductsService();
 
-router.get('/',async (req, res) => {
+router.get('/', async (req, res) => {
   const products = await service.find();
   res.json(products);
 });
@@ -13,39 +19,45 @@ router.get('/filter', (req, res) => {
   res.send('Yo soy u filter');
 });
 
-router.get('/:id', async (req, res, next) => {
+router.get('/:id',
+  validatorHandler(getProductSchema, 'params'),
+  async (req, res, next) => {
   try {
     const { id } = req.params;
     const product = await service.findOne(id);
     res.json(product);
   } catch (error) {
-    next(error)
+    next(error);
   }
 });
 
-router.post('/', async (req, res) => {
+router.post('/', 
+  validatorHandler(createProductSchema, 'body'),
+  async (req, res) => {
   const body = req.body;
-  const newProduct = await service.create(body)
+  const newProduct = await service.create(body);
   res.status(201).json(newProduct);
 });
 
-router.patch('/:id', async (req, res) => {
+router.patch('/:id', 
+  validatorHandler(getProductSchema, 'params'),
+  validatorHandler(updateProductSchema, 'body'),
+  async (req, res, next) => {
   try {
     const { id } = req.params;
     const body = req.body;
     const product = await service.update(id, body);
     res.json(product);
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    next(error);
   }
 });
 
-router.delete('/:id',async (req, res) => {
+router.delete('/:id', async (req, res) => {
   const { id } = req.params;
-  const rta =await service.delete(id);
+  const rta = await service.delete(id);
 
   res.json(rta);
 });
-
 
 module.exports = router;
